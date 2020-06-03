b54108ad2836d03964aeff515da307494318b68e
adil-82 <bouaddiadil82@gmail.com>
2020-06-04 00:32:52 +0200

    Seconde commmit

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use('/', require('./routes/index'))
 app.use('/users', require('./routes/users'))
 
 
-const PORT = process.env.PORT || 5002
+const PORT = process.env.PORT || 5003
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
