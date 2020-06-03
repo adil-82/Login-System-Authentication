f88d10ca9129f50e0e790dcbe2cc7b9c7a4835bf
adil-82 <bouaddiadil82@gmail.com>
2020-06-03 23:40:36 +0200

    Initial commit

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const UseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        default: Date.now
+    },
+})
+
+
+module.exports = mongoose.model('User', UseSchema)
